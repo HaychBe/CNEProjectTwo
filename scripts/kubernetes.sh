e1e914fb08f193503e8e696a82fb1dd119f4b017
Ubuntu <ubuntu@ip-172-31-47-206.eu-west-2.compute.internal>
2021-04-08 19:27:55 +0000

    added scripts

---
+++ scripts/kubernetes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# download
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+# make executable chmod +x kubectl
+chmod +x ./kubectl
+# install binary in to your PATH
+sudo mv ./kubectl /usr/local/bin/kubectl 
---
+++ scripts/mysql.sh
@@ -0,0 +1,1 @@
+sudo apt update -y && sudo apt install mysql-client-core-5.7 -y
